bf43c312e3f8793bf51d1ec9bbeb6a4150c9e2c2
Augusta916306 <Augusta.okeke@edu.sait.ca>
2023-10-24 14:18:54 -0600

    step3: database

--- sample.py
+++ sample.py
@@ -11,5 +11,12 @@ theme = input("Enter the theme: ")
 language = input("Enter the langage: ")
 auto_save = bool('auto_save')  
 
+#Database settings
+host = input("Enter the localhost: ")
+port = int("Enter the port: ")
+username = input("Enter your username: ")
+password = input("Enter your password: ")
+
 print(f"My name is {name} I am {age} years old my email address = {email}")
-print(f"theme ={theme} language = {language} auto_save = {auto_save}")
+print(f"theme = {theme} language = {language} auto_save = {auto_save}")
+print(f"host = {host} port = {port} username = {username} password= {password}")
