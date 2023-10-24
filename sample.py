5606eaa05fa14bba67131d5616c82a2dff4fdc66
Augusta916306 <Augusta.okeke@edu.sait.ca>
2023-10-24 09:40:25 -0600

    Step1: General settings

---
+++ sample.py
@@ -0,0 +1,6 @@
+#General settings to get user input
+name = input("Enter your name: ")
+age = input("How old are you?: ")
+email = input("Enter your email address: ")
+
+print(f"My name is {name} I am {age} years old my email address = {email}")
